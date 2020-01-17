e3c949129d947fd38fab2cfbe245d50134418f9b
Aaron Culp <aaron_culp@anfcorp.com>
2020-01-17 13:57:13 -0500

    added geolocation to state

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 
-const App = () => {
-    window.navigator.geolocation.getCurrentPosition (
-        (position) => console.log(position),
-        (err) => console.log(err)
-    );
-    return (
-        <SeasonDisplay />
-    )
+export default class App extends Component {
+    state = {
+        location: [],
+    }
+
+    componentDidMount() {
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => this.setState({location: position.coords.latitude}),
+            (err) => console.log(err)
+        );
+    }s
+
+    render() {
+        return ( 
+            <div>Latitude: {this.state.location}</div>
+        )
+    }
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render( < App / > , document.getElementById('root'));
