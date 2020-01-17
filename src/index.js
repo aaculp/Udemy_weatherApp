1e1237a59f5a77e9303f93190a297d9bb8e7e88a
Aaron Culp <aaron_culp@anfcorp.com>
2020-01-17 14:22:37 -0500

    added in ternary operator for loading data

--- src/index.js
+++ src/index.js
@@ -4,20 +4,39 @@ import SeasonDisplay from './SeasonDisplay';
 
 export default class App extends Component {
     state = {
-        location: [],
+        location: null,
+        errorMessage: null,
     }
 
     componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
             (position) => this.setState({location: position.coords.latitude}),
-            (err) => console.log(err)
+            (err) => this.setState({errorMessage: err.message})
         );
     }s
 
     render() {
-        return ( 
-            <div>Latitude: {this.state.location}</div>
-        )
+        // option 1
+        if(this.state.errorMessage && !this.state.location) {
+            return <div>Error: {this.state.errorMessage}</div>;
+        }
+
+        if(!this.state.errorMessage && this.state.location) {
+            return <div>Latitude: {this.state.latitude}</div>;
+        }
+
+        return <div>Loading!</div>
+
+        // option 2
+        // let message = (this.state.errorMessage && !this.state.location) ? 
+        //     `Error: ${this.state.errorMessage}` : 
+        //     `Latitude: ${this.state.location}`; 
+
+        // return (
+        //     <div>
+        //         {message}
+        //     </div>
+        // )
     }
 }
 
