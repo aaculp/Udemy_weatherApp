362582674669896af82406cbae3cc0e65d22f07d
Aaron Culp <aaron_culp@anfcorp.com>
2020-01-17 10:45:35 -0500

    first commit with two components and geolocation added

---
+++ src/SeasonDisplay.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const SeasonDisplay = () => {
+    return (
+        <div>
+            Season Display 
+        </div>
+    )
+}
+
+export default SeasonDisplay
