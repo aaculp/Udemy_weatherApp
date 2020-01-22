e4537cb4a6f7c88fe1f6d99e488ada82af847c81
Aaron Culp <aaron_culp@anfcorp.com>
2020-01-21 20:56:47 -0500

    added function to SeasonDisplay

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
+const getSeason = (lat, month) => {
+    if (month > 2 && month < 9 ) {
+        lat > 0 ? 'summer' : 'winter';
+    } else {
+        lat > 0 ? 'winter' : 'summer';
+
+    }
+}
+
 const SeasonDisplay = (props) => {
+    const season = getSeason(props.lat, new Date().getMonth)
     return (
         <div>Latitude: {props.latitude}</div>
     )
