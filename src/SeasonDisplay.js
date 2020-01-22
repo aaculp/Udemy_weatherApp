d24f5fa02bb38c6de1e5eba5ab551b2c31a11d5d
Aaron Culp <aaron_culp@anfcorp.com>
2020-01-21 21:05:48 -0500

    fixed up SeasonDisplay component

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -3,15 +3,16 @@ import ReactDom from 'react-dom';
 
 const getSeason = (lat, month) => {
     if (month > 2 && month < 9 ) {
-        lat > 0 ? 'summer' : 'winter';
+        return lat > 0 ? 'summer' : 'winter';
     } else {
-        lat > 0 ? 'winter' : 'summer';
+        return lat > 0 ? 'winter' : 'summer';
 
     }
 }
 
 const SeasonDisplay = (props) => {
-    const season = getSeason(props.lat, new Date().getMonth)
+    const season = getSeason(props.lat, new Date().getMonth());
+    console.log(season)
     return (
         <div>Latitude: {props.latitude}</div>
     )
