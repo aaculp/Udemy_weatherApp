0b83ab7c37cede208fb47c0fe0611714477bcb40
Aaron Culp <aaron_culp@anfcorp.com>
2020-01-27 21:36:15 -0500

    added styles and stylesheet

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,3 +1,4 @@
+import './SeasonDisplay.css';
 import React from 'react';
 import ReactDom from 'react-dom';
 
@@ -13,7 +14,7 @@ const seasonConfig = {
 }
 
 const getSeason = (lat, month) => {
-    if (month > 2 && month < 9 ) {
+    if (month > 0 && month < 9 ) {
         return lat > 0 ? 'summer' : 'winter';
     } else {
         return lat > 0 ? 'winter' : 'summer';
@@ -29,10 +30,10 @@ const SeasonDisplay = (props) => {
     // Destructing the obj seasonConfig
     const { text, iconName } = seasonConfig[season]
     return (
-        <div>
-            <i className = {`${iconName} icon`} />
+        <div className = {`season-display ${season}`} >
+            <i className = {`icon-left massive ${iconName} icon`} />
             <h1> { text } </h1>
-            <i className = {`${iconName} icon`} />
+            <i className = {`icon-right massive ${iconName} icon`} />
         </div>
     )
 }
